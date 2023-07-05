7119fc27614d7af55222641bb9ccb9688787c8d5
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-07-04 21:24:14 -0700

    replaced fetchTheBacon API with manually input texts

--- scripts/index.js
+++ scripts/index.js
@@ -12,38 +12,48 @@ const game = {
   timeoutId: null,
   runFirstAnimation: false,
   runFinalAnimation: false,
-  chosenHero: 'fire',
+  chosenHero: 'leaf',
   loopDuration: 1,
   selectedOption: null,
+  currentMode: 'easy', 
+  currentRound: '1', 
+  numberOfParagraph: 2,
   fetchTheBacon: () => {
-    $.getJSON(
-      "https://baconipsum.com/api/?callback=?",
-      { type: "all-meat", "start-with-lorem": "0", paras: "1" },
-      function (baconGoodness) {
-        if (baconGoodness && baconGoodness.length > 0) {
-          $("#target").html("");
-          for (let i = 0; i < baconGoodness.length; i++) {
-            game.targetWords = baconGoodness[i].split(/\s+/).filter(word => word.trim() !== "");
-            // Count total words and add to totalWords property
-            game.totalWords = game.targetWords.length;
-            $("#target").append("<p>");
-            // now loop through each character 
-            for (let j = 0; j < baconGoodness[i].length; j++) {
-              if (baconGoodness[i][j] !== " " || (j > 0 && baconGoodness[i][j - 1] !== " ")) {
-                $("#target").append(`<span>${baconGoodness[i][j]}</span>`);
-                game.targetText.push(baconGoodness[i][j]);
-              }
-            }
-  
-            $("#target").append("</p>");
-            // these 2 lines of codes are for highlighting the first letter when the paragraph was generated
-            $("#target span:eq(0)").addClass("cursorPosition");
-            $("#target span:eq(0)").addClass("cursorPosition-bkg");
+    let paragraph = '';
+    // Set the conditions to generate the paragraph based on the mode and round
+    if (game.currentMode === 'easy' && game.currentRound === '1') {
+      paragraph = game.text50[Math.floor(Math.random() * game.numberOfParagraph)];
+    } else if ((game.currentMode === 'easy' && game.currentRound === '2') || (game.currentMode === 'normal' && game.currentRound === '1')){
+      paragraph = game.text70[Math.floor(Math.random() * game.numberOfParagraph)];
+    } else if ((game.currentMode === 'easy' && game.currentRound === '3') || (game.currentMode === 'normal' && game.currentRound === '2') || (game.currentMode === 'hard' && game.currentRound === '1')) {
+      paragraph = game.text100[Math.floor(Math.random() * game.numberOfParagraph)];
+    } else if ((game.currentMode === 'normal' && game.currentRound === '3') || (game.currentMode === 'hard' && game.currentRound === '2')){
+      paragraph = game.text120[Math.floor(Math.random() * game.numberOfParagraph)];
+    } else if (game.currentMode === 'hard' && game.currentRound === '3') {
+      paragraph = game.text150[Math.floor(Math.random() * game.numberOfParagraph)];
+    }
+    if (paragraph && paragraph.length > 0) {
+      $("#target").html("");
+      
+        game.targetWords = paragraph.split(/\s+/).filter(word => word.trim() !== "");
+        // Count total words and add to totalWords property
+        game.totalWords = game.targetWords.length;
+        $("#target").append("<p>");
+        // now loop through each character 
+        for (let j = 0; j < paragraph.length; j++) {
+          if (paragraph[j] !== " " || (j > 0 && paragraph[j - 1] !== " ")) {
+            $("#target").append(`<span>${paragraph[j]}</span>`);
+            game.targetText.push(paragraph[j]);
           }
         }
-      }
-    );
-  },
+
+      $("#target").append("</p>");
+      // these 2 lines of codes are for highlighting the first letter when the paragraph was generated
+      $("#target span:eq(0)").addClass("cursorPosition");
+      $("#target span:eq(0)").addClass("cursorPosition-bkg");
+    };
+    
+    },
   
   handleKeyup: function (event) {
     console.log("key up", event, event.key);
@@ -306,6 +316,28 @@ const game = {
       });
     });
   },
+
+  text50: [
+    "Armed with a gleaming sword and unwavering resolve, the hero ventured into the treacherous unknown. Perilous mountains and haunted forests stood in their path, but they pressed on, fueled by courage and the call of destiny. They faced fearsome beasts and conquered daunting trials, emerging as a legend whose tale would endure.",
+    "In a world yearning for salvation, a hero emerged from the shadows, driven by a burning desire to protect the innocent. With lightning-fast reflexes and a heart ablaze with righteousness, they battled dark forces that threatened to engulf the realm. Their valiant deeds etched their name in history, inspiring generations to come."
+  ],
+
+  text70: [
+    "Amidst a realm gripped by darkness, a hero emerged, driven by an unyielding sense of justice. Their path was fraught with peril as they journeyed through treacherous lands and faced malevolent creatures. With a sword of gleaming steel and a heart ablaze with determination, they vanquished evil, rekindling hope in the hearts of the oppressed. Legends would sing of their valor, forever immortalizing their name in the annals of heroism.",
+    "From humble beginnings, a hero's destiny unfolded. They embarked on a quest, guided by ancient prophecies and the whispered tales of sages. Across vast landscapes, they encountered allies and foes alike, honing their skills and forging an indomitable spirit. Through trials and tribulations, they rose as a beacon of hope, inspiring others to embrace their own heroic journey, forever changing the course of their world."
+  ],
+  text100: [
+    "In a realm where dreams took flight, a land brimming with enchantment, a wondrous fairy tale unfolded. Whimsical creatures danced beneath moonlit skies, their laughter echoing through ancient woods. Pixies adorned petals with sparkling dewdrops, casting a spell of ethereal beauty. Majestic castles stood tall, guarded by noble knights with hearts pure as the morning sun. Princesses, with flowing gowns and kind hearts, awaited their destined heroes. Legends spoke of magical mirrors and everlasting love. Amidst mystical realms, wishes whispered on gentle breezes, secrets hidden in mythical forests. Within this realm, where imagination reigned, fairy tales were woven, bringing joy and wonder to all who believed.",
+    "In a world painted with magic hues, a tapestry of enchanting tales unfurled. Forests whispered secrets to the wind, while mystical creatures frolicked in dappled glades. Beneath a moonlit sky, fairies weaved spells of wonder, their delicate wings shimmering with ethereal light. Brave adventurers embarked on quests, guided by the wisdom of ancient sorcerers and the melodies of enchanted flutes. Princes battled wicked sorceresses, their love a beacon of hope in the darkness. Castles guarded cherished treasures, while unicorns pranced through emerald meadows. Within this realm, where dreams danced and wishes came true, fairy tales cast their spell, painting the world with pure enchantment."
+  ],
+  text120: [
+    "Once upon a time, in a world drenched in enchantment and wonder, fairy tales wove their timeless narratives. Towering castles loomed over emerald landscapes, their turrets reaching towards the heavens. Noble knights mounted their gallant steeds, embarking on daring quests to vanquish evil and rescue damsels in distress. Benevolent fairies sprinkled shimmering pixie dust, granting wishes and spreading joy. Mythical creatures roamed enchanted forests, where talking animals imparted wisdom to those who would listen. Dragons breathed flames, their scales glistening like jewels in the sunlight. Wicked witches weaved spells of malevolence, their cackles echoing through the night. And amidst this tapestry of magic, brave heroes and heroines confronted their fears, found love, and discovered their destinies, reminding us all that within the realms of imagination, dreams become reality.",
+    "In a realm where fantasy and reality intertwined, fairy tales unfurled their mesmerizing tales. Beneath starlit skies, resplendent castles stood as symbols of grandeur and aspiration. Heroes and heroines embarked on epic odysseys, wielding swords of destiny against formidable adversaries. Gentle fairies flitted on gossamer wings, granting blessings of love and happiness. Lush forests teemed with mystical creatures, their eyes gleaming with ancient wisdom. Dragons soared through clouds, breathing fire that illuminated the heavens. Enchanting spells cast by wise sorcerers illuminated hidden truths and tested the limits of courage. And in this realm, dreams blossomed into extraordinary adventures, reminding us that within each of us lies the power to believe in magic, embrace our destinies, and create our own happily ever after."
+  ],
+  text150: [
+    "In a realm where the extraordinary merged with the ordinary, a tapestry of enchantment unfurled. Magic crackled in the air, swirling in vibrant hues, as wizards and sorceresses cast spells that defied the laws of nature. Luminous fairies flitted among ancient forests, their delicate wings leaving trails of iridescent dust. Majestic creatures—unicorns with silver manes, phoenixes ablaze with resplendent plumage—graced the landscape, their presence a testament to the realm's ethereal beauty. Hidden within the heart of the land stood enchanted citadels, their spires reaching towards the heavens. Whispers of forgotten incantations echoed through the corridors of time, while mystical artifacts held immeasurable power. It was a world where dreams took flight, where imagination kindled the spark of possibility. In this realm of wonder, both dazzling and elusive, each moment was infused with the potential for magical transformation—a realm where the veil between the mystical and the tangible was forever thin.",
+    "Step into a world where the veil of reality is gently lifted, revealing a realm of captivating magic. Cascading waterfalls glimmer with golden essence, their droplets imbued with extraordinary properties. Energetic sprites dart among sunlit meadows, their playful laughter filling the air. Ancient trees, their branches laden with ancient wisdom, whisper secrets only the attentive can decipher. Ethereal beings, shimmering with light, traverse the skies, leaving trails of stardust in their wake. Enchanted crystals pulse with raw power, harnessed by skilled sorcerers for noble pursuits. Each step reveals hidden doorways to enchanted realms, where time bends and dreams take tangible form. It is a world of limitless possibility, where the ordinary is infused with the extraordinary, and where the boundary between reality and imagination dissolves into a symphony of wonder. In this magical world, the wonders of the cosmos are but a touch away, and the essence of magic dances in harmony with the pulse of life itself."
+  ],
 };
 
 $(game.init);
